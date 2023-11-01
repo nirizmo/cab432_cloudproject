6be0ab10f5d9546a279eda58cd711c077bc405d6
nirizmo <nirizmo@gmail.com>
2023-11-01 15:27:00 +1000

    Update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const stream = require('stream');
 const AWS = require('aws-sdk');
 const app = express();
 const path = require('path'); // Import the path module
+const fs = require('fs');
 
 // Configure AWS S3
 AWS.config.update({
@@ -48,24 +49,22 @@ app.use(express.json());
 
 // Function to Upload & Transcode
 app.post('/upload', upload.single('videoFile'), (req, res) => {
+  
   const format = req.body.format;
   const bitrate = req.body.bitrate;
   const resolution = req.body.resolution;
   const generateThumbnail = req.body.generateThumbnail;
 
   let videoCodec;
-  let fileExtension;
 
   if (format === 'mp4') {
     videoCodec = 'libx264';
-    fileExtension = 'mp4';
-  } else if (format === 'mkv') {
-    videoCodec = 'libx265';
-    fileExtension = 'mkv';
-  } else {
-    // Default values in case format is not recognized
+  } else if (format === 'mov') {
     videoCodec = 'libx264';
-    fileExtension = 'mp4';
+  } else if (format === 'avi') {
+    videoCodec = 'libxvid';
+  } else {
+    videoCodec = 'libx264';
   }
 
   console.log("1. File Uploading...");
@@ -87,15 +86,9 @@ app.post('/upload', upload.single('videoFile'), (req, res) => {
       return res.status(500).send('Failed to upload the original file to S3');
     }
 
-    //const inputVideoPath = path.join('uploads/', req.file.originalname);
-    //const outVideoPath = path.join('uploads/', req.file.originalname);
-
-    //const formatExtension = format === 'mp4' ? 'mp4' : 'mkv';
-    const outVideoPath = path.join('uploads/', `${path.basename(req.file.originalname, path.extname(req.file.originalname))}.${fileExtension}`);
-
+    const outVideoPath = path.join('tmp/', `${path.basename(req.file.originalname, path.extname(req.file.originalname))}.${format}`);
     const ffmpegPath = path.join(__dirname, 'ffmpeg', 'ffmpeg'); // Assuming 'ffmpeg.exe' is in a 'ffmpeg' subdirectory of your root directory
 
-    // Now you can use 'ffmpegPath' to reference the FFmpeg executable in your Node.js application
     console.log('Path to FFmpeg:', ffmpegPath);
 
     let readableVideoBuffer = new stream.PassThrough();
@@ -104,6 +97,7 @@ app.post('/upload', upload.single('videoFile'), (req, res) => {
 
     ffmpeg(readableVideoBuffer)
       .setFfmpegPath(ffmpegPath)
+      .outputFormat(format)
       .videoCodec(videoCodec)
       .audioCodec('aac')
       .audioBitrate(bitrate)
@@ -115,15 +109,19 @@ app.post('/upload', upload.single('videoFile'), (req, res) => {
       .on('end', () => {
         console.log("4. Video transcoded");
         // Transcoding complete
-        if (generateThumbnail) {
-          // Generate a thumbnail here if needed
-        }       
+        
+        function bufferFile(relPath) {
+          return fs.readFileSync(relPath); // zzzz....
+        }
+
+        const transcodedFolder = 'transcodes/';
+        const transcodedFileKey = transcodedFolder + `${path.basename(req.file.originalname, path.extname(req.file.originalname))}.${format}`;
 
         // Upload the transcoded video to S3
         const transcodedFileParams = {
           Bucket: s3BucketName,
-          Key: 'transcoded/' + req.file.originalname,
-          Body: uploadedFileBuffer, // Use the buffer of the uploaded file
+          Key: transcodedFileKey,
+          Body: bufferFile(outVideoPath), // Use the buffer of the uploaded file
         };
 
         console.log("5. Transcoded video uploaded");
@@ -146,6 +144,16 @@ app.post('/upload', upload.single('videoFile'), (req, res) => {
           });
         });
         console.log("6. Process completed");
+
+        try {
+          // Synchronously delete the local video file in the "tmp" directory
+          fs.unlinkSync(outVideoPath);
+          console.log('Local video file deleted');
+        } catch (err) {
+          console.error('Error deleting local video file:', err);
+        }
+
+        
       })
       .on('error', (err) => {
         console.error('FFmpeg error:', err);
